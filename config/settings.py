69973cfa3c53dd064e927e54f82369912be74e91
amahadnezhad <amahadnezhad@gmail.com>
2023-02-06 17:45:25 +0330

    Create Pages App For Home & AboutUs

--- config/settings.py
+++ config/settings.py
@@ -38,8 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     # Local Apps
     'accounts.apps.AccountsConfig',
+    'pages.apps.PagesConfig',
 ]
 
 MIDDLEWARE = [
