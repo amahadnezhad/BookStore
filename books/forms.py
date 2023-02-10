fce71e0364dfcf07c8475ec6595d083bef17287f
amahadnezhad <amahadnezhad@gmail.com>
2023-02-10 16:49:31 +0330

    Add CommentForm To bookdetailview

---
+++ books/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+
+from .models import Book, Comment
+
+
+class BookForm(forms.ModelForm):
+    class Meta:
+        model = Book
+        fields = ['title', 'author', 'description', 'price', 'cover', ]
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['text', 'recommend', ]
--- books/views.py
+++ books/views.py
@@ -3,7 +3,7 @@ from django.urls import reverse_lazy
 from django.shortcuts import get_object_or_404, render
 
 from .models import Book
-from .form import BookForm
+from .forms import BookForm , CommentForm
 
 
 class BookListView(generic.ListView):
@@ -19,9 +19,23 @@ class BookListView(generic.ListView):
 
 def book_detail_view(request, pk):
     book = get_object_or_404(Book, pk=pk)
-    comments = book.comments.all()
+    comments = book.comments.filter(is_active=True)
 
-    return render(request, 'books/bookdetail.html', context={'book': book, 'comments': comments})
+    if request.method == 'POST':
+        comment_form = CommentForm(request.POST)
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False)
+            new_comment.book = book
+            new_comment.user = request.user
+            new_comment.save()
+            comment_form = CommentForm()
+    else:
+        comment_form = CommentForm()
+
+    return render(request, 'books/bookdetail.html', context={'book': book,
+                                                             'comments': comments,
+                                                             'comment_form': comment_form,
+                                                             })
 
 
 class BookCreateView(generic.CreateView):
